4b52288f26cd1d7e0be7684b4b03ff6e7068449d
DESKTOP-HI964HF\tmonet <breakboy0526@gmail.com>
2021-11-03 17:20:36 +0900

    해시 주석주가

--- hash/Sha256.java
+++ hash/Sha256.java
@@ -8,8 +8,12 @@ public class Sha256 {
 
 	// 해싱
 	public String hashing(String message) throws NoSuchAlgorithmException {
+		// 1. 메시지 입력
 		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		// 2. 메시지 해싱
 		md.update(message.getBytes());
+
+		// 3. Base64 인코딩
 		String result = Base64.getEncoder().encodeToString(md.digest());
 
 		return result;
